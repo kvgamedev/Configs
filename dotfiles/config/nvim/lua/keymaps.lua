dcca851ae6ae21b671871f1c02ddc2aec10cc9ac
kvgamedev <kuberrv2005@gmail.com>
2025-08-03 00:04:25 +0530

    NVIM: Added titles to floating windows and removed Escape x2 to exit terminal mode

--- dotfiles/config/nvim/lua/keymaps.lua
+++ dotfiles/config/nvim/lua/keymaps.lua
@@ -1,5 +1,4 @@
 local map = vim.keymap.set
-map({ "t" }, "<esc><esc>", "<c-\\><c-n>", { desc = "Exit Terminal Mode" })
 map({ "n", "v", "o" }, "<A-l>", "G", { desc = "Last Line" })
 map({ "n", "v", "o" }, "<A-h>", "gg", { desc = "First Line" })
 map({ "n", "v", "o" }, "<S-l>", "$", { desc = "Last Character" })
--- dotfiles/config/nvim/lua/scripts/floaterm.lua
+++ dotfiles/config/nvim/lua/scripts/floaterm.lua
@@ -13,10 +13,8 @@ local function createFloatingWindow(opts)
 	opts = opts or {}
 	local width = opts.width or math.floor(vim.o.columns * 0.8)
 	local height = opts.height or math.floor(vim.o.lines * 0.8)
-
 	local col = math.floor((vim.o.columns - width) / 2)
 	local row = math.floor((vim.o.lines - height) / 2)
-
 	local buf = -1
 	if vim.api.nvim_buf_is_valid(opts.buf) then
 		buf = opts.buf
@@ -26,25 +24,25 @@ local function createFloatingWindow(opts)
 
 	local win_config = {
 		relative = "editor",
+		title = opts.title or "Floating Winodw",
 		width = width,
 		height = height,
 		col = col,
 		row = row,
 		style = "minimal",
 		border = "rounded",
+		title_pos = opts.title_pos or "center",
 	}
-
 	local win = vim.api.nvim_open_win(buf, true, win_config)
+
 	return { buf = buf, win = win }
 end
 
-vim.keymap.set("n", "<leader>o", "<Cmd>source lua/scripts/floaterm.lua<Cr>", { desc = "Source FloatTerm" })
-vim.keymap.set({ "n", "t" }, "<leader>wt", "<cmd>FloatTerm<cr>", { desc = "FloatTerm" })
-vim.keymap.set({ "n", "t" }, "<leader>wg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
-
+-- FloatTerm
 vim.api.nvim_create_user_command("FloatTerm", function()
 	if not vim.api.nvim_win_is_valid(state.float_term.win) then
-		state.float_term = createFloatingWindow({ buf = state.float_term.buf })
+		state.float_term = createFloatingWindow({ buf = state.float_term.buf, title = "Float Term" })
+		vim.api.nvim_set_option_value("buflisted", false, { buf = state.float_term.buf })
 		if vim.bo[state.float_term.buf].buftype ~= "terminal" then
 			vim.cmd("terminal")
 		end
@@ -54,9 +52,11 @@ vim.api.nvim_create_user_command("FloatTerm", function()
 	end
 end, {})
 
+-- LazyGit
 vim.api.nvim_create_user_command("LazyGit", function()
-	state.lazy_git = createFloatingWindow({ buf = state.lazy_git.buf })
+	state.lazy_git = createFloatingWindow({ buf = state.lazy_git.buf, title = "LazyGit" })
 	vim.bo[state.lazy_git.buf].filetype = "lazygit"
+	vim.api.nvim_buf_set_name(state.lazy_git.buf, "LazyGit")
 	vim.fn.jobstart("lazygit", {
 		term = true,
 		on_exit = function()
@@ -66,3 +66,8 @@ vim.api.nvim_create_user_command("LazyGit", function()
 	})
 	vim.cmd("startinsert")
 end, {})
+
+-- Keymaps
+vim.keymap.set("n", "<leader>o", "<Cmd>source lua/scripts/floaterm.lua<Cr>", { desc = "Source FloatTerm" })
+vim.keymap.set({ "n", "t" }, "<leader>wt", "<cmd>FloatTerm<cr>", { desc = "FloatTerm" })
+vim.keymap.set({ "n", "t" }, "<leader>wg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
