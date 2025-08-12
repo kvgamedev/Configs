6f411631ccc8d6790efbfe3198980ad3afd42e1c
kvgamedev <kuberr2005@gmail.com>
2025-08-12 20:16:31 +0530

    Corrected --walker-dir to --walker-root : config.fish

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,11 +1,12 @@
 if status is-interactive
+	alias cat 'bat'
 	alias ls 'eza --icons auto'
 	alias cd 'z'
 	alias cf 'cd (fzf --walker=dir,hidden)'
 	alias vim 'nvim'
 	alias v 'nvim'
 	alias vi 'nvim (fzf)'
-	alias ch 'cd (fzf --walker=dir,hidden --walker-dir=$HOME)'
+	alias ch 'cd (fzf --walker=dir,hidden --walker-root=$HOME)'
 
 	starship init fish | source
 	zoxide init fish | source
