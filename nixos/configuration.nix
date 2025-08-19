2a165fa824832086c214f2ce4a622b97c4a1b535
kvgamedev <kuberrv2005@gmail.com>
2025-08-19 14:56:21 +0530

    moved nix config to git repo and then ln -sf it at /etc/nixos/

---
+++ nixos/configuration.nix
@@ -0,0 +1,163 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+imports =
+	[ # Include the results of the hardware scan.
+	./hardware-configuration.nix
+	];
+
+# Bootloader.
+boot.loader.systemd-boot.enable = true;
+boot.loader.efi.canTouchEfiVariables = true;
+
+# Use latest kernel.
+boot.kernelPackages = pkgs.linuxPackages_latest;
+
+networking.hostName = "nixos"; # Define your hostname.
+# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+networking.networkmanager.enable = true;
+
+time.timeZone = "Asia/Kolkata";
+i18n.defaultLocale = "en_IN";
+i18n.extraLocaleSettings = {
+	LC_ADDRESS = "en_IN";
+	LC_IDENTIFICATION = "en_IN";
+	LC_MEASUREMENT = "en_IN";
+	LC_MONETARY = "en_IN";
+	LC_NAME = "en_IN";
+	LC_NUMERIC = "en_IN";
+	LC_PAPER = "en_IN";
+	LC_TELEPHONE = "en_IN";
+	LC_TIME = "en_IN";
+};
+
+services.xserver.xkb = {
+	layout = "us";
+	variant = "";
+};
+
+users.users.kv = {
+	isNormalUser = true;
+	description = "KV";
+	extraGroups = [ "networkmanager" "wheel" ];
+	packages = with pkgs; [];
+	shell = pkgs.fish;
+};
+
+nixpkgs.config.allowUnfree = true;
+
+# $ nix search wget
+environment.systemPackages = with pkgs; [
+	neovim
+	hyprland
+	kitty
+	ghostty
+	git
+	git-lfs
+	lazygit
+	fzf
+	brightnessctl
+	noto-fonts
+	noto-fonts-emoji
+	yazi
+	fish
+	starship
+	keyd
+	rofi-wayland
+	unzip
+	zoxide
+	eza
+	bat
+	ripgrep
+	fd
+	brave
+	wl-clipboard
+	cliphist
+	bottom
+
+	# Development
+	gnumake
+	cmake
+	gcc
+
+	zig
+	zls
+
+	glfw
+	imgui
+
+	# Ricing
+	waybar
+	matugen
+	swww
+
+];
+
+fonts.packages = with pkgs; [
+	nerd-fonts.noto
+	noto-fonts-color-emoji
+];
+
+programs.hyprland.enable = true;
+environment.sessionVariables.NIXOS_OZONE_WL = "1";
+programs.fish.enable = true;
+
+services.keyd = {
+	enable = true;
+	keyboards = {
+		default = {
+			ids = [ "*" ];
+			settings = {
+				main = {
+					capslock = "timeout(escape, 70, leftcontrol)";
+					escape = "capslock";
+				};
+				otherlayer = {};
+			};
+			extraConfig = ''
+				'';
+		};
+	};
+};
+
+environment.variables.EDITOR = "nvim";
+environment.variables.VISUAL = "nvim";
+programs.neovim = {
+	enable = true;
+	defaultEditor = true;
+	viAlias = true;
+	vimAlias = true;
+};
+
+# programs.nix-ld.enable = true;
+# programs.nix-ld.libraries = with pkgs; [
+# ];
+
+nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+# programs.mtr.enable = true;
+# programs.gnupg.agent = {
+#   enable = true;
+#   enableSSHSupport = true;
+# };
+
+# List services that you want to enable:
+
+# Enable the OpenSSH daemon.
+# services.openssh.enable = true;
+
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+# networking.firewall.enable = false;
+
+system.stateVersion = "25.05";
+}
