d048478392e756e3cd079de0cf5a86f49108dd36
kvgamedev <kuberrv2005@gmail.com>
2025-08-21 14:13:03 +0530

    nvim: removed manually adding lsp capabilities since blink.setup already does that

--- configs/nvim/lua/plugins/blink.lua
+++ configs/nvim/lua/plugins/blink.lua
@@ -2,8 +2,6 @@ vim.pack.add({
 	{ src = "https://github.com/Saghen/blink.cmp", version = vim.version.range("*") },
 })
 
-vim.lsp.client.capabilities = require("blink.cmp").get_lsp_capabilities()
-
 require("blink.cmp").setup({
 	completion = { documentation = { auto_show = true } },
 	sources = {
