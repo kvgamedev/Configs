c6447a781451ccf7700323cfb1fb5db8c5b17def
kvgamedev <kuberrv2005@gmail.com>
2025-08-23 22:22:41 +0530

    nvim: use custom plugin manager

--- configs/nvim/lua/plugins/init.lua
+++ configs/nvim/lua/plugins/init.lua
@@ -1,28 +1,58 @@
-vim.pack.add({
-	"https://github.com/shortcuts/no-neck-pain.nvim",
-	"https://github.com/echasnovski/mini.nvim",
-	"https://github.com/ibhagwan/fzf-lua",
-	"https://github.com/folke/flash.nvim",
-	"https://github.com/nvim-lualine/lualine.nvim",
-	"https://github.com/neovim/nvim-lspconfig",
-	"https://github.com/nvim-treesitter/nvim-treesitter",
-	"https://github.com/lewis6991/gitsigns.nvim",
-	"https://github.com/folke/which-key.nvim",
-	{ src = "https://github.com/Saghen/blink.cmp", version = vim.version.range("*") },
-	"https://github.com/stevearc/conform.nvim",
-	-- "https://github.com/folke/zen-mode.nvim", -- Doesn't play well with mini.tabline
-}, { load = true, confirm = false })
-
+local plugman = require("custom.plugin_manager")
 local configs = require("plugins.configs")
 
-configs.noNeckPain()
-configs.mini()
-configs.fzf()
-configs.flash()
-configs.lualine()
-configs.lspconfig()
-configs.treesitter()
-configs.gitsigns()
-configs.whichkey()
-configs.blink()
-configs.conform()
+plugman.addplug({
+	src = "https://github.com/shortcuts/no-neck-pain.nvim",
+	post_hook = configs.noNeckPain,
+})
+
+plugman.addplug({
+	src = "https://github.com/echasnovski/mini.nvim",
+	post_hook = configs.mini,
+})
+
+plugman.addplug({
+	src = "https://github.com/ibhagwan/fzf-lua",
+	post_hook = configs.fzf,
+})
+
+plugman.addplug({
+	src = "https://github.com/folke/flash.nvim",
+	post_hook = M.flash,
+})
+
+plugman.addplug({
+	src = "https://github.com/nvim-lualine/lualine.nvim",
+	post_hook = configs.lualine,
+})
+
+plugman.addplug({
+	src = "https://github.com/neovim/nvim-lspconfig",
+	post_hook = configs.lspconfig,
+})
+
+plugman.addplug({
+	src = "https://github.com/nvim-treesitter/nvim-treesitter",
+	post_hook = configs.treesitter,
+})
+
+plugman.addplug({
+	src = "https://github.com/lewis6991/gitsigns.nvim",
+	post_hook = configs.gitsigns,
+})
+
+plugman.addplug({
+	src = "https://github.com/folke/which-key.nvim",
+	post_hook = configs.whichkey,
+})
+
+plugman.addplug({
+	src = "https://github.com/Saghen/blink.cmp",
+	version = "*",
+	post_hook = configs.blink,
+})
+
+plugman.addplug({
+	src = "https://github.com/stevearc/conform.nvim",
+	post_hook = configs.conform,
+})
