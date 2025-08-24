c5f32dabd487431e84ccd22ede647583e1450a17
kvgamedev <kuberrv2005@gmail.com>
2025-08-24 19:37:34 +0530

    nvim: use addplug to add colorscheme plugins

--- configs/nvim/lua/plugins/colorscheme.lua
+++ configs/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,24 @@
 local plugman = require("custom.plugin_manager")
-plugman.addplug("https://github.com/ellisonleao/gruvbox.nvim")
-plugman.addplug({src = "https://github.com/catppuccin/nvim", name = "catppuccin" })
 
-require("gruvbox").setup({
-	transparent_mode = true,
-	bold = false,
-	contrast = "hard",
+plugman.addplug({
+	src = "https://github.com/ellisonleao/gruvbox.nvim",
+	post_hook = function()
+		require("gruvbox").setup({
+			transparent_mode = true,
+			bold = false,
+			contrast = "hard",
+		})
+	end,
 })
-require("catppuccin").setup({
-	transparent_background = true,
+
+plugman.addplug({
+	src = "https://github.com/catppuccin/nvim",
+	name = "catppuccin",
+	post_hook = function()
+		require("catppuccin").setup({
+			transparent_background = true,
+		})
+	end,
 })
 
 vim.cmd("colorscheme catppuccin")
